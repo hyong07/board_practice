fa2f09a2fcbd0f634ad8b89d399cb469d2250bda
Unknown <love__65ss@naver.com>
2018-07-19 16:12:44 +0900

    로그인, 회원가입 완료

--- src/main/java/kh/board/impl/IMemberService.java
+++ src/main/java/kh/board/impl/IMemberService.java
@@ -1,7 +1,27 @@
 package kh.board.impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import kh.board.dto.MemberDTO;
+import kh.board.interfaces.MemberDAO;
 import kh.board.interfaces.MemberService;
 
+@Component
 public class IMemberService implements MemberService{
-
+	
+	@Autowired
+	private MemberDAO dao;
+		
+	@Override
+	public int insertMem(MemberDTO dto) {
+		return this.dao.insertMem(dto);
+	}
+	
+	@Override
+	public List<MemberDTO> loginMem(MemberDTO dto) {
+		return this.dao.loginMem(dto);
+	}
 }
--- src/main/java/kh/board/interfaces/MemberDAO.java
+++ src/main/java/kh/board/interfaces/MemberDAO.java
@@ -1,5 +1,10 @@
 package kh.board.interfaces;
 
-public interface MemberDAO {
+import java.util.List;
 
+import kh.board.dto.MemberDTO;
+
+public interface MemberDAO {
+	public int insertMem(MemberDTO dto);
+	public List<MemberDTO> loginMem(MemberDTO dto);
 }
